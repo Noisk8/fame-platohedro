c850b8013e85702e937ee277634f2cb34b376319
igor <igor@localhost.localdomain>
2024-07-16 00:48:00 -0500

    basico

---
+++ api/index.tsx
@@ -0,0 +1,40 @@
+import { Button, Frog,  } from "frog";
+import { devtools } from "frog/dev";
+import { serveStatic } from "frog/serve-static";
+// import { neynar } from 'frog/hubs'
+import { handle } from "frog/vercel";
+
+// Uncomment to use Edge Runtime.
+// export const config = {
+//   runtime: 'edge',
+// }
+
+export const app = new Frog({
+  assetsPath: '/',
+  basePath :'/api',
+  title:'Platohedro'
+
+})
+
+
+
+app.frame('/', (c) => {
+  const { } = c
+  return c.res({
+
+  
+    image: "/public/fuma.jpg",
+    intents: [
+      <Button.Link href="https://platohedro.org/">Platohedro.org</Button.Link>,
+      <Button.Link href="https://www.patreon.com/platohedro">Patreon</Button.Link>,
+      <Button.Link href="https://platohedro.org/donaciones/">Donaciones</Button.Link>,
+    ]
+  })
+})
+// @ts-ignore
+const isEdgeFunction = typeof EdgeFunction !== "undefined";
+const isProduction = isEdgeFunction || import.meta.env?.MODE !== "development";
+devtools(app, isProduction ? { assetsPath: "/.frog" } : { serveStatic });
+
+export const GET = handle(app);
+export const POST = handle(app);
